e8a8c1ea67cacc3f66ba93262111bfd8ad4bbb71
ArtyomTimofeev <nifegdeil@gmail.com>
2022-02-03 16:20:50 +0500

    add class List manager #JC19-256

--- vanilla/src/api/api.ts
+++ vanilla/src/api/api.ts
@@ -1,5 +1,5 @@
 import { getAuth } from 'firebase/auth';
-import { query, collection, getDocs, orderBy } from 'firebase/firestore';
+import { query, collection, getDocs, orderBy, QuerySnapshot, DocumentData } from 'firebase/firestore';
 
 import { FILMS_COLLECTION } from '../utils/constants';
 import { FilmsSortingType } from '../entities/enums/filmSortingTypeEnum';
@@ -15,9 +15,66 @@ export const auth = getAuth();
  * Gets film list from collection.
  * @param sortingType - Type of selected sort.
  */
+
 export const getFilms = async(sortingType = FilmsSortingType.Default): Promise<Film[]> => {
   const queryForFilms = query(collection(db, FILMS_COLLECTION), orderBy(sortingType, 'asc'));
   const filmsSnapshot = await getDocs(queryForFilms);
   const filmDocuments = filmsSnapshot.docs.map(doc => doc.data() as FilmDocumentDTO);
   return filmDocuments.map(filmDocument => filmMapper.fromDto(filmDocument));
 };
+
+interface FetchOptions<T> {
+  readonly startFrom: T | null;
+  readonly sort?: string;
+}
+
+type FetchFunctionType<T> = (options: FetchOptions<T>) => Promise<T[]>;
+export class ListManager<T> {
+  private readonly fetchDataFunc: FetchFunctionType<T>;
+
+  private lastVisibleElement: T | null = null;
+
+  private sort?: string;
+
+  private readonly allItems: T[] = [];
+
+  private readonly pageSize = 10;
+
+  private currentPage = 1;
+
+  public constructor(fetchFunction: FetchFunctionType<T>) {
+    this.fetchDataFunc = fetchFunction;
+  }
+
+  public async nextPage(): Promise<T[]> {
+    const list = await this.fetchDataFunc({
+      startFrom: this.lastVisibleElement,
+      sort: this.sort,
+    });
+    this.currentPage++;
+    this.allItems.push(...list);
+
+    this.lastVisibleElement = list[list.length - 1];
+
+    return list;
+  }
+
+  public prevPage(): T[] {
+    if (this.currentPage === 1) {
+      return this.allItems;
+    }
+    this.currentPage--;
+    const start = this.currentPage * this.pageSize;
+    return this.allItems.splice(start, this.pageSize);
+  }
+
+  public setSort(): void {
+    this.allItems.splice(0, this.allItems.length);
+    this.currentPage = 1;
+  }
+}
+
+const filmsListManager = new ListManager(options => getFilms(options.sort as any));
+filmsListManager.nextPage();
+
+filmsListManager.setSort();
