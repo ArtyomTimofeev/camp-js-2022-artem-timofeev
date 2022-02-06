c77ef6addea6a23c8705811c7ed5fcb281962bbf
ArtyomTimofeev <nifegdeil@gmail.com>
2022-02-06 19:40:03 +0500

    delete excess comments #JC19-256

--- vanilla/src/api/authorization.ts
+++ vanilla/src/api/authorization.ts
@@ -1,27 +1,19 @@
 import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 
 import { Authorization } from './../entities/models/authorization';
+
 export const authorization: Authorization = {
 
   isUserAuthorized: false,
 
-  /**
-   * Login through google account.
-   * @param auth - Function from firebase.
-   */
   async login(auth) {
     const provider = new GoogleAuthProvider();
     await signInWithPopup(auth, provider);
     this.isUserAuthorized = true;
   },
 
-  /**
-   * Logging out of google account.
-   * @param auth - Function from firebase.
-   */
   async logout(auth) {
     await signOut(auth);
     this.isUserAuthorized = false;
   },
-
 };
