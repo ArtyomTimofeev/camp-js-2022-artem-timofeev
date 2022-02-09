959341750bcbb2a635932404877a07b77346e388
Maxim <korkinwow@gmail.com>
2022-02-10 01:18:45 +0700

    typos #JC19-278

--- vanilla/src/utils/debounce.ts
+++ vanilla/src/utils/debounce.ts
@@ -1,8 +1,8 @@
 /**
  * Function for delaying the execution of the callback and redelaying it if it was called before previous timer expired.
  * @param callback - Function which should be delayed.
- * @param delay - Miliseconds of delay.
- * @returns Function which executes callback and redelay it.
+ * @param delay - Miliseconds of delay. Default value is 1000ms.
+ * @returns Function which executes callback and redelays it.
  */
 export function debounce<Params extends any[]>(callback: (...args: Params) => unknown, delay = 1000): Function {
   let timeoutId: number | null = null;
